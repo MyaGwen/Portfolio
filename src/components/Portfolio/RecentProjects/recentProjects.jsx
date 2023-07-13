573c702d677a2d6110ae647f7406e4b88fb46609
MyaGwen <wendy.e.ahadome@gmail.com>
2023-07-13 23:22:25 +0000

    images update

---
+++ src/components/Portfolio/RecentProjects/recentProjects.jsx
@@ -0,0 +1,71 @@
+import gameVault from "../../../assets/images/gamevault.png";
+import checkIt from "../../../assets/images/check it.png";
+import BFF from "../../../assets/images/dog.png";
+import BB from "../../../assets/images/BB scoreboard.png";
+import Messi from "../../../assets/images/messi.png";
+import Travel from "../../../assets/images/travel.png";
+
+export { gameVault, checkIt, BFF, BB, Messi, Travel };
+
+export const htmlCss = [
+  {
+    image: gameVault,
+    title: "Game vault",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css, js",
+    responsive: true,
+    preview: "https://game-vault-mu.vercel.app/",
+    code: "https://github.com/MyaGwen/GameVault.git",
+  },
+  {
+    image: checkIt,
+    title: "check it",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css, js",
+    responsive: true,
+    preview: "https://check-it-gamma.vercel.app/",
+    code: "https://github.com/MyaGwen/Todo-list.git",
+  },
+  {
+    image: BFF,
+    title: "Man's BFF",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css, js",
+    responsive: true,
+    preview: "https://mans-bff-gallery.netlify.app/",
+    code: "https://github.com/MyaGwen/Image-Gallery.git",
+  },
+  {
+    image: BB,
+    title: "Basketball Scoreboard",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css, js",
+    responsive: true,
+    preview: "https://tourmaline-selkie-6b5806.netlify.app/",
+    code: "https://github.com/MyaGwen/Basket-Ball-ScoreBoard.git",
+  },
+  {
+    image: Messi,
+    title: "google search result of messi",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css",
+    responsive: false,
+    preview: "https://gilded-horse-a1a41a.netlify.app/",
+    code: "https://github.com/MyaGwen/Stutern-Messi-google-search-result.git",
+  },
+  {
+    image: Travel,
+    title: "travel website",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi aperiam quia dolor vel pariatur?",
+    stack: "html, css",
+    responsive: false,
+    preview: "https://rainbow-clafoutis-d0bf85.netlify.app/",
+    code: "https://github.com/MyaGwen/Travel-website.git",
+  },
+];
